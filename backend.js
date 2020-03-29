a0ca8a5f58b12dc0ca9161b2f6634995806f7f2e
Ada Lerner <ada.r.lerner@gmail.com>
2020-03-29 15:15:23 -0400

    First version of backend.js, with update event.
    Sends state on connection/broadcasts state on update.

---
+++ backend.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+//const http = require('http').createServer(app);
+const port = 3000;
+
+var state = {
+  'pieces': [
+    {
+      'name': 'Glorious Hammer',
+      'id': 1,
+      'color': '#abcdea',
+      'initiative': 20,
+      'acted': false
+    }
+  ]
+};
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  // Send new users the game state immediately
+  socket.emit('update', state);
+
+  // When we receive an update from the client...
+  // 1. Update the server's state
+  // 2. Broadcast the new state to all clients
+  socket.on('update', function(msg) {
+    try {
+      let newState = JSON.parse(msg);
+      state = newState
+      console.log('JSON.parsed a new state:');
+      console.log(newState);
+      io.emit('update', state);
+    } catch (e) {
+      console.log(e);
+      console.log('Could not JSON.parse this: ' + msg);
+    }
+  })
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
