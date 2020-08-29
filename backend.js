af61b9df02ba2197379eb65d49864d529b598911
Ada Lerner <ada.r.lerner@gmail.com>
2020-08-29 14:57:12 -0400

    Fix all pieces prior to sending them out
    Removes all fields that shouldn't be there.
    Adds all fields (with default values) that are missing.

--- backend.js
+++ backend.js
@@ -16,6 +16,43 @@ try {
 console.log('Starting state');
 console.log(state);
 
+const REQUIRED_FIELDS = {
+  id: undefined,
+  initiative: 0,
+  acted: false,
+  name: 'XXX NO NAME XXX',
+  color: '#000000',
+  peripheralMotes: 0,
+  personalMotes: 0,
+  wp: 5,
+  notes: '',
+  mostRecentCrashRecovery: undefined,
+  mostRecentCrash: undefined,
+  hadActedAtCrash: undefined
+}
+
+const fixPiece = (piece) => {
+  console.log(`Fixing piece ${piece.name}`)
+  Object.keys(REQUIRED_FIELDS).forEach((k, i) => {
+    if (!piece.hasOwnProperty(k)) {
+      piece[k] = REQUIRED_FIELDS[k]; // Set to default value
+      console.log(`Setting field ${k} to ${piece[k]}`)
+    }
+  });
+  Object.keys(piece).forEach((k, i) => {
+    if (!REQUIRED_FIELDS.hasOwnProperty(k)) {
+      console.log(`Removing field ${k}`)
+      delete piece[k];  // Remove fields not in the template
+    }
+  });
+}
+
+const fixGameState = (gameState) => {
+  gameState.pieces.forEach((piece, i) => {
+    fixPiece(piece);
+  });
+}
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -27,6 +64,7 @@ io.on('connection', function(socket){
   console.log('a user connected');
 
   // Send new users the game state immediately
+  fixGameState(state);
   socket.emit('update', state);
 
   // When we receive an update from the client...
@@ -38,6 +76,7 @@ io.on('connection', function(socket){
       state = newState
       console.log('Received a new state from a client:');
       console.log(newState);
+      fixGameState(state);
       socket.broadcast.emit('update', state);
 
       try {
